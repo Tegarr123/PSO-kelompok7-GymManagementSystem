824799509a0c358bb53866833a9abfcdcd83595c
Tegarr123 <fadhino987@gmail.com>
2024-11-28 21:19:46 +0700

    add cleanup artifact

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'gym-db'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'gym'
