aea68d0403adb5b9c2028bf7b642b4fd90578558
Tegarr123 <fadhino987@gmail.com>
2024-12-11 16:29:55 +0700

    clean-up repo

--- commands/get-secret-grafana.sh
+++
@@ -1,1 +0,0 @@
-kubectl get secret --namespace prometheus stable-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo 
--- commands/install eksctl.sh
+++
@@ -1,7 +0,0 @@
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-cd /tmp
-sudo mv /tmp/eksctl /bin
-eksctl version
-
-# setup kubernetes cluster
-eksctl create cluster --name gym-pso-cluster --region us-east-1 --node-type t2.small --nodes 3
